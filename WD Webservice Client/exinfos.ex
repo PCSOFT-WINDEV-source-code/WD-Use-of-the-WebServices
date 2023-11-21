243ad8b098e4d376d2556498b845423bec57a783
PCSOFT-WINDEV <appli@pcsoft.fr>
2023-11-21 18:27:03 +0100

    Initial Commit

---
+++ WD Webservice Client/exinfos.ex
@@ -0,0 +1,8 @@
+[INFOS]
+gaf=0
+title=WD Webservice Client
+image=description.gif
+description=description.rtf
+source=0
+[OBJECT]
+WD Webservice Client.WDP=1
---
+++ WD Webservice Server/exinfos.ex
@@ -0,0 +1,8 @@
+[INFOS]
+gaf=0
+title=WD Webservice Server
+image=description.gif
+description=description.rtf
+source=0
+[OBJECT]
+WD Webservice Server.WDP=1
